3856a1e5010f634d8b26d2a3f7b94ca0079d4d31
Zayan <muhammad.zayan@jbsapplify.com>
2025-06-01 10:26:22 +0500

    - check deploy

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -41,7 +41,7 @@ const UserList = () => {
                 rel="noreferrer"
                 className="inline-block mt-4 text-sm text-blue-500 hover:underline"
               >
-                🌐 Visit the Website
+                🌐 Visit the Websiteeeeeeeeeeeeeeee
               </a>
             </div>
           ))}
