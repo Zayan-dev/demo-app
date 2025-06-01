6e43a376b16082402b83ca86dce49192aeb3d538
Zayan <muhammad.zayan@jbsapplify.com>
2025-06-01 09:38:13 +0500

    Fix GitHub Pages deploy for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/demo-app/", // 👈 this must match your repo name
   plugins: [react()],
-  base: "/demo-app/",
 });
